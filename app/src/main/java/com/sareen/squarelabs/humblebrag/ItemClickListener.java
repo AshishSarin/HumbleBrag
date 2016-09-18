a468f71d9045ef98c05233fc42163561557dee11
AshishSarin <asareen007@gmail.com>
2016-09-18 18:00:11 +0530

    ItemClickListener for RecyclerView items

---
+++ app/src/main/java/com/sareen/squarelabs/humblebrag/ItemClickListener.java
@@ -0,0 +1,14 @@
+package com.sareen.squarelabs.humblebrag;
+
+import android.view.View;
+
+/**
+ * Created by Ashish on 18-09-2016.
+ */
+
+/* This interface is created for click listener for
+   recyclerview items
+*/
+public interface ItemClickListener {
+    void onClick(View view, int position);
+}
